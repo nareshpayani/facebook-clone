0e217099a6f1942b22c01948a8af4e738b15e767
Naresh Payani <nareshpayani@Nareshs-iMac.local>
2020-09-18 14:55:50 +0530

    Project CleanUp

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAYAOhy0O-1EiG7czTJyrQdQBnCDuB1Fy8",
+    authDomain: "facebook-clone-3f970.firebaseapp.com",
+    databaseURL: "https://facebook-clone-3f970.firebaseio.com",
+    projectId: "facebook-clone-3f970",
+    storageBucket: "facebook-clone-3f970.appspot.com",
+    messagingSenderId: "920532155866",
+    appId: "1:920532155866:web:7c6b6cbee2cea6f1de54e3",
+    measurementId: "G-8H19JCDKF0"
+  };
